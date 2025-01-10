2cab306e43e3927dd2f8deae6a807e792c889ec6
RigelTLT <kozyrin-di@yandex.ru>
2025-01-10 16:25:48 +0400

    fix: fix function scrollUp

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import * as bootstrap from "bootstrap";
 import "./style.scss";
-import { loadHeader, loadFooter, loadMap } from "./components/global/global";
+import {
+  loadHeader,
+  loadFooter,
+  loadMap,
+  scrollUp,
+} from "./components/global/global";
 
 document.addEventListener("DOMContentLoaded", async () => {
   await loadHeader();
   await loadFooter();
   await loadMap();
-});
-
-function scrollUp() {
-  document.body.scrollIntoView({ behavior: "smooth" });
-}
-const scrollBtn = document.querySelector(".btn-up-link");
-
-scrollBtn.addEventListener("click", function () {
   scrollUp();
 });
